bba169e23b26bac66ca88615d19c3386354e82e2
Pheenoh <pheenoh@gmail.com>
2020-12-28 10:56:52 -0500

    add support for pulling github release tags and patches

---
+++ wasm/static/fetch-github-releases.js
@@ -0,0 +1,28 @@
+var releases = new Map();
+
+window.onload = function () {
+  
+  const url = "https://api.github.com/repos/hallcristobal/tpgz/releases";
+
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      for (const release of data) {
+        let downloadUrls = new Map();
+        for (const url of release.assets) {
+          downloadUrls.set(url.name, url.browser_download_url);
+        }
+        releases.set(release.tag_name, downloadUrls);
+      }
+
+      let keys = [...releases.keys()];
+
+      let options = keys
+        .map((item) => `<option value=${item.toLowerCase()}>${item}</option>`)
+        .join("\n");
+      document.querySelector("select").innerHTML = options;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
